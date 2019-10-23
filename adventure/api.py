2c4654b92ca22553acb427a7566956e6c6851ca7
kjohna <43828763+kjohna@users.noreply.github.com>
2019-10-23 17:40:31 -0600

    add room exits to init and move responses

--- adventure/api.py
+++ adventure/api.py
@@ -22,7 +22,17 @@ def initialize(request):
     uuid = player.uuid
     room = player.room()
     players = room.playerNames(player_id)
-    return JsonResponse({'uuid': uuid, 'name': player.user.username, 'title': room.title, 'description': room.description, 'players': players}, safe=True)
+    # get room exits
+    exits = []
+    if room.n_to:
+        exits.append('n')
+    if room.s_to:
+        exits.append('s')
+    if room.e_to:
+        exits.append('e')
+    if room.w_to:
+        exits.append('w')
+    return JsonResponse({'uuid': uuid, 'name': player.user.username, 'title': room.title, 'description': room.description, 'players': players, 'exits': exits}, safe=True)
 
 
 # @csrf_exempt
@@ -89,10 +99,21 @@ def move(request):
         #     pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} has walked {dirs[direction]}.'})
         # for p_uuid in nextPlayerUUIDs:
         #     pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} has entered from the {reverse_dirs[direction]}.'})
-        return JsonResponse({'name': player.user.username, 'title': nextRoom.title, 'description': nextRoom.description, 'players': players, 'error_msg': ""}, safe=True)
+        # get room exits
+        exits = []
+        if room.n_to:
+            exits.append('n')
+        if room.s_to:
+            exits.append('s')
+        if room.e_to:
+            exits.append('e')
+        if room.w_to:
+            exits.append('w')
+        return JsonResponse({'uuid': uuid, 'name': player.user.username, 'title': room.title, 'description': room.description, 'players': players, 'exits': exits}, safe=True)
+        return JsonResponse({'name': player.user.username, 'title': nextRoom.title, 'description': nextRoom.description, 'players': players, 'exits': exits, 'error_msg': ""}, safe=True)
     else:
         players = room.playerNames(player_id)
-        return JsonResponse({'name': player.user.username, 'title': room.title, 'description': room.description, 'players': players, 'error_msg': "You cannot move that way."}, safe=True)
+        return JsonResponse({'name': player.user.username, 'title': room.title, 'description': room.description, 'players': players, 'exits': exits, 'error_msg': "You cannot move that way."}, safe=True)
 
 
 # @csrf_exempt
